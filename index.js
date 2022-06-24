d631af88c8cc833158f2f6dfbee0c7e140a83c71
klaw178 <klaw178@gmail.com>
2022-06-24 11:29:52 +0100

    added css animations on icons

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+// on scroll navbar color change
+
 $(window).scroll(function () {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
 });
+
+// on scroll icons animations
+
+const serviceIconsObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate__animated', 'animate__bounceIn');
+        }
+    });
+});
+
+const contactIconsObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate__animated', 'animate__tada');
+        }
+    });
+});
+
+let serviceIcons = document.querySelectorAll('.service-icon');
+let contactIcons = document.querySelectorAll('.contact-icon');
+
+for (let icon of serviceIcons) {
+    serviceIconsObserver.observe(icon);
+}
+
+for (let icon of contactIcons) {
+    contactIconsObserver.observe(icon);
+}
+
